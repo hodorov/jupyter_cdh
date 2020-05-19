065d1f748241e30113ccb5b5ff37f9c59d5a4ffe
el1191 <el1191@woyd.ru>
2020-05-19 13:22:16 +0300

    Move --allow-root to extra opts, add custom Anaconda binary path

--- jupyter-csd/scripts/control.sh
+++ jupyter-csd/scripts/control.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 CMD=$1
 
-ANACONDA_BIN_DIR=$(dirname "${CDH_PYTHON}")
+if [ -z "$ANACONDA_BIN_DIR" ]; then
+  ANACONDA_BIN_DIR=$(dirname "${CDH_PYTHON}")
+fi
 export PATH=$ANACONDA_BIN_DIR:$PATH
 
 case $CMD in
   (start)
     echo "Starting the Jupyter Notebook on port [$JPY_NOTEBOOK_PORT]"
-    exec $ANACONDA_BIN_DIR/jupyter notebook --no-browser --ip 0.0.0.0 --port $JPY_NOTEBOOK_PORT --allow-root
+    exec $ANACONDA_BIN_DIR/jupyter notebook --no-browser --ip 0.0.0.0 --port $JPY_NOTEBOOK_PORT $JPY_EXTRA_OPTS
     ;;
   (*)
     echo "Don't understand [$CMD]"
